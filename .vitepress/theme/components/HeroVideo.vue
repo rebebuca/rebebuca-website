54c6f53a3e4f177e7dcae468c3a1eef6917041cd
godkun <godkun666@126.com>
2024-02-14 15:22:12 +0800

    feat: add bei-an

--- .vitepress/theme/components/HeroVideo.vue
+++ .vitepress/theme/components/HeroVideo.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <video class="home-video" src="/quick.mp4" controls="controls"></video>
+  <video class="home-video" poster="/poster.png" src="/quick.mp4" controls="controls"></video>
 </template>
 
 <style>
 .home-video {
   margin-left: 60px;
   margin-top: 42px;
+  background: transparent;
 }
 
 @media (max-width: 960px) {
